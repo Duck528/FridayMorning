7cd94b51cd154a1e3f8ca03cb46f6b68aff8b319
DeockHwan Anh <dhanh0528@gmail.com>
2019-02-10 02:18:11 +0900

    first commit

---
+++ lib/Home/home.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _backgroundImage(),
+        _loverInfoWidget(),
+        _coupleDetailInfoWidget(),
+        _floatingActionButton(),
+      ],
+    );
+  }
+
+  Container _backgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/default_bg.jpg'),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Widget _loverInfoWidget() {
+    Container _spacing(double space) {
+      return Container(
+        width: space,
+      );
+    }
+
+    Text configureBoldText(String text) {
+      return Text(text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ));
+    }
+
+    return SafeArea(
+      child: Align(
+        alignment: AlignmentDirectional.topStart,
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.healing),
+                  _spacing(5.0),
+                  configureBoldText('Suvely'),
+                ])),
+      ),
+    );
+  }
+
+  Widget _floatingActionButton() {
+    return Align(
+      alignment: Alignment(0.9, 0.95),
+      child: FloatingActionButton(
+        backgroundColor: Colors.redAccent,
+        child: Icon(Icons.add),
+        onPressed: _floatingButtonTapped,
+      ),
+    );
+  }
+
+  void _floatingButtonTapped() {}
+
+  Widget _coupleDetailInfoWidget() {
+    Text configureMainText(String text) {
+      return Text(text, style: TextStyle(color: Colors.white, fontSize: 28));
+    }
+
+    Text configureSubText(String text) {
+      return Text(text, style: TextStyle(color: Colors.white, fontSize: 16));
+    }
+
+    Text configureMinorText(String text) {
+      return Text(text, style: TextStyle(color: Colors.white, fontSize: 14));
+    }
+
+    return Align(
+      alignment: AlignmentDirectional.bottomStart,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(40, 0, 0, 0),
+        child: Container(
+          height: 220,
+          width: 250,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              configureMainText('4117 days'),
+              configureSubText('지금까지 우리 함께한 날 들'),
+              Divider(color: Colors.white),
+              configureMinorText('25번째 생일'),
+              configureMinorText('??일 남았습니다')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'Home/home.dart';
+
+void main() => runApp(FridayMorningApp());
+
+class FridayMorningApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Friday Morning',
+      theme: ThemeData.dark(),
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  final List<Widget> _tapWidgets = [
+    HomePage(),
+    Text('Hello Feed'),
+    Text('Photo Albums'),
+    Text('Settings')
+  ];
+  int _currentIndex = 0;
+
+  void _tapBarItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _tapBarItemTapped,
+        items: [
+          BottomNavigationBarItem(title: Text('Home'), icon: Icon(Icons.home)),
+          BottomNavigationBarItem(title: Text('Feed'), icon: Icon(Icons.hd)),
+          BottomNavigationBarItem(title: Text('Photo'), icon: Icon(Icons.photo)),
+          BottomNavigationBarItem(title: Text('Settings'), icon: Icon(Icons.settings))
+        ],
+      ),
+      body: Center(child: _tapWidgets[_currentIndex],),
+    );
+  }
+}
