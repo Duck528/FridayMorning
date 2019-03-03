fb6b80847f7d7b17b6604e2401490a3019a839a0
DeockHwan Anh <dhanh0528@gmail.com>
2019-03-03 14:29:17 +0900

    세부 UI 작업

--- lib/SignIn/signin_page.dart
+++ lib/SignIn/signin_page.dart
@@ -58,7 +58,9 @@ class _SignInWidgetState extends State<_SignInWidget> {
           _configureSubTitle('Pick id for display you on friday morning.'),
           spacing(15),
           _configureInputID('Enter ID'),
-          _idEntered ? _configureInputPW('Enter Password', context) : Container()
+          _idEntered
+              ? _configureInputPW('Enter Password', context)
+              : Container()
         ],
       ),
     );
@@ -83,7 +85,7 @@ class _SignInWidgetState extends State<_SignInWidget> {
     return Container(
         height: 74,
         child: TextField(
-          onSubmitted: (text) { 
+          onSubmitted: (text) {
             setState(() {
               _idEntered = true;
             });
@@ -91,30 +93,47 @@ class _SignInWidgetState extends State<_SignInWidget> {
           cursorColor: Colors.black,
           style: notoSansBoldFont(24, Colors.black),
           decoration: InputDecoration(
-              suffixIcon: Icon(Icons.check, color: Color.fromRGBO(222, 222, 222, 1),),
+              suffixIcon: Icon(
+                Icons.check,
+                color: Color.fromRGBO(222, 222, 222, 1),
+              ),
               labelText: placehold,
-              enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
-              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
+              enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
               labelStyle:
                   notoSansBoldFont(22, Color.fromRGBO(222, 222, 222, 1))),
         ));
   }
 
-    Widget _configureInputPW(String placehold, BuildContext context) {
+  Widget _configureInputPW(String placehold, BuildContext context) {
     return Container(
         height: 74,
         child: TextField(
           obscureText: true,
-          onSubmitted: (text) { 
-            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => WelcomePage()));
+          onSubmitted: (text) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => WelcomePage()));
           },
           cursorColor: Colors.black,
           style: notoSansBoldFont(24, Colors.black),
           decoration: InputDecoration(
-              suffixIcon: Icon(Icons.check, color: Color.fromRGBO(222, 222, 222, 1),),
+              suffixIcon: Icon(
+                Icons.check,
+                color: Color.fromRGBO(222, 222, 222, 1),
+              ),
               labelText: placehold,
-              enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
-              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
+              enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(222, 222, 222, 1))),
               labelStyle:
                   notoSansBoldFont(22, Color.fromRGBO(222, 222, 222, 1))),
         ));
@@ -159,32 +178,45 @@ class _NestStepWidget extends StatelessWidget {
 
   Widget _configureNextButton(BuildContext context) {
     return FlatButton(
-      highlightColor: Colors.white70,
-      onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => WelcomePage()));
-      },
-      child: Stack(children: <Widget>[
-        Positioned(
-          top: 16,
-          left: 30,
-          bottom: 17,
-          child: Text(
-            'NEXT',
-            style: notoSansBoldFont(17, Colors.white),
+        highlightColor: Colors.white70,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => WelcomePage()));
+        },
+        child: Stack(children: <Widget>[
+          Positioned(
+              top: 16,
+              left: 30,
+              bottom: 17,
+              child: Text(
+                'NEXT',
+                style: notoSansBoldFont(17, Colors.white),
+              )),
+          Positioned(
+            top: 16,
+            bottom: 16,
+            right: 24,
+            child: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.white,
+            ),
           )
-        ),
-        Positioned(
-          top: 16,
-          bottom: 16,
-          right: 24,
-          child: Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.white,
-          ),
-        )
-      ])
+        ]));
+  }
+}
+
+class _WarningWidget extends StatelessWidget {
+  final String _invalidIdMessage = 'The ID is not valid';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 36,
+      child: Text(
+        _invalidIdMessage,
+        style: notoSansRegularFont(15, Colors.white),
+      ),
     );
   }
-
-  Widget _configureStepIndicator() {}
 }
